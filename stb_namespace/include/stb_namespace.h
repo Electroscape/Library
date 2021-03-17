e5ff0f3f221265a87b2399c9e263ed1144cc8640
RPi3-Lab <abdullahsaei@hotmail.com>
2021-03-17 14:55:01 +0000

    add initSerials and camelCase format

--- stb_namespace/include/stb_namespace.h
+++ stb_namespace/include/stb_namespace.h
@@ -1,22 +1,26 @@
 #pragma once
 
 #include <Arduino.h>
+#include <PCF8574.h> /* I2C Port Expander  */
+#include <Wire.h>    /* I2C library */
 
+// Defined by hardware
+#define RELAY_I2C_ADD 0x3F
 #define MAX_CTRL_PIN A0
 #define MAX485_WRITE HIGH
 #define MAX485_READ LOW
 
-namespace stb_namespace
-{
-    class Brain
-    {
-    private:
-        /* data */
-    public:
-        Brain(String BrainName);
-        ~Brain();
-    };
-        
-    void printWithHeader(String message, String source);
-    void software_Reset();
-}
+namespace stb_namespace {
+class Brain {
+   private:
+    /* data */
+   public:
+    Brain(String BrainName);
+    ~Brain();
+};
+
+void printWithHeader(String message, String source);
+void softwareReset();
+bool i2cScanner();
+bool brainSerialInit();
+}  // namespace stb_namespace
--- stb_namespace/src/stb_namespace.cpp
+++ stb_namespace/src/stb_namespace.cpp
@@ -1,35 +1,66 @@
 #include <stb_namespace.h>
 using namespace stb_namespace;
 
-stb_namespace::Brain::Brain(String BrainName)
-{
+stb_namespace::Brain::Brain(String BrainName) {
 }
 
-stb_namespace::Brain::~Brain()
-{
+stb_namespace::Brain::~Brain() {
 }
 
-void stb_namespace::printWithHeader(String message, String source)
-{
-  digitalWrite(MAX_CTRL_PIN, MAX485_WRITE);
-  Serial.println();
-  Serial.print("!Br,");
-  Serial.print(source);
-  Serial.print(",");
-  Serial.print(message);
-  Serial.println(",Done.");
-  delay(50);
-  digitalWrite(MAX_CTRL_PIN, MAX485_READ);
-}
-
-void stb_namespace::software_Reset()
-{
-  Serial.println(F("Restarting in"));
-  delay(50);
-  for (byte i = 3; i > 0; i--)
-  {
-    Serial.println(i);
+bool stb_namespace::brainSerialInit() {
+    Wire.begin();
+    Serial.begin(115200);
     delay(100);
-  }
-  asm volatile("  jmp 0");
+    // initialize the read pin as an output:
+    pinMode(MAX_CTRL_PIN, OUTPUT);
+    // Welcome Print
+    printWithHeader("!Setup Begin", "SYS");
+    return true;
+}
+
+void stb_namespace::printWithHeader(String message, String source) {
+    digitalWrite(MAX_CTRL_PIN, MAX485_WRITE);
+    Serial.println();
+    Serial.print("!Br,");
+    Serial.print(source);
+    Serial.print(",");
+    Serial.print(message);
+    Serial.println(",Done.");
+    delay(50);
+    digitalWrite(MAX_CTRL_PIN, MAX485_READ);
+}
+
+void stb_namespace::softwareReset() {
+    Serial.println(F("Restarting in"));
+    delay(50);
+    for (byte i = 3; i > 0; i--) {
+        Serial.println(i);
+        delay(100);
+    }
+    asm volatile("  jmp 0");
+}
+
+bool stb_namespace::i2cScanner() {
+    Serial.println();
+    Serial.println(F("I2C scanner:"));
+    Serial.println(F("Scanning..."));
+    byte count = 0;
+    for (byte i = 8; i < 120; i++) {
+        Wire.beginTransmission(i);
+        if (Wire.endTransmission() == 0) {
+            Serial.print("Found address: ");
+            Serial.print(i, DEC);
+            Serial.print(" (0x");
+            Serial.print(i, HEX);
+            Serial.println(")");
+            count++;
+            delay(1);  // maybe unneeded?
+        }              // end of good response
+    }                  // end of for loop
+    Serial.println("Done.");
+    Serial.print("Found ");
+    Serial.print(count, DEC);
+    Serial.println(" device(s).");
+
+    return true;
 }
